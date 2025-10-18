8c5153247acde7c6e38a45484ae576a522d11783
Tobias Krizansky <tobiaskrizan@gmail.com>
2025-10-18 20:20:11 +0200

    Initial Commit

---
+++ TestBLE2/TestBLE2App.swift
@@ -0,0 +1,17 @@
+//
+//  TestBLE2App.swift
+//  TestBLE2
+//
+//  Created by Tobias Krizansky on 18.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestBLE2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
